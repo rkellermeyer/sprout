931285dc05662ca36ff01b0065d9ce2d6058dfdb
rkellermeyer <rnkconcepts@gmail.com>
2012-09-06 22:35:41 -0400

    prepped for heroku

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sprout::Application.config.secret_token = 'ad306facb54bdbfbcee957908f9c01d8f15306bd9b9eca7e5fd2340eca310be85e0666c1c858d4fe3cbc93d8ebbc748d2ec2340e8e57e9aff0a166830656a21d'
